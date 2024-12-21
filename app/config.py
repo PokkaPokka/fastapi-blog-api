c66b22456627f47de983a9d902c3baa365d8ee92
Yinuo Wu <scott.wyn1215@gmail.com>
2024-12-20 20:38:25 -0600

    updated env

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     DB_HOST: str
-    SQLALCHEMY_DATABASE_URL: str
+    DATABASE_URL: str
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
@@ -12,5 +13,6 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
