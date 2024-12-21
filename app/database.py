9cc45414606b0e78bfecabede2a1819edb9f8d3a
Yinuo Wu <scott.wyn1215@gmail.com>
2024-12-20 20:31:26 -0600

    updated heroku env

--- app/database.py
+++ app/database.py
@@ -6,9 +6,9 @@ import os
 
 load_dotenv()
 
-URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+URL = os.getenv("DATABASE_URL")
 if URL is None:
-    raise ValueError("No SQLALCHEMY_DATABASE_URL found in environment variables")
+    raise ValueError("No DATABASE_URL found in environment variables")
 
 engine = create_engine(URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
